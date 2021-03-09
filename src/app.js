ff4f646d8395311f506c66599c98991081b40811
MarKru21 <77971128+MarKru21@users.noreply.github.com>
2021-03-09 19:32:16 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ function displayTemperature(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=9fdfde34a67a648a41ee1aa53553e730&units=metric";
